70a49cea6ff32b3e7af01cd432ba611ca6f25022
sbx_user1051 <sbx_user1051@169.254.3.227>
2025-05-30 07:59:08 +0000

    Fri May 30 07:59:08 UTC 2025

--- record_and_upload.sh
+++ record_and_upload.sh
@@ -10,12 +10,12 @@ PARAM_SOURCECODE_DIR="${SCRIPT_CURRENT_DIR}"
 
 # For MacOS newer than 11.0.0  - allow the terminal to run the Java runtime
 xattr -rd com.apple.quarantine "${SCRIPT_CURRENT_DIR}" >/dev/null 2>&1 || true
--Djava.net.preferIPv4Stack=true   \
 
 echo "Running using packaged JRE:"
 set -ex
 exec "$JAVA_BIN"                    \
   -Dlogback.enableJansi="true"      \
+  -Djava.net.preferIPv4Stack=true   \
   -jar "$JAR_FILE"                  \
   "--config" "${PARAM_CONFIG_FILE}" \
   "--store" "${PARAM_STORE_DIR}"    \
